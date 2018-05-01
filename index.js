bd134b45c598dd742b7762ade297e5a3f80c6801
heidi_laptop_home <hmlanden@gmail.com>
2018-05-01 00:13:07 -0700

    add filters that don't work

--- index.js
+++ index.js
@@ -2,27 +2,38 @@
 var $tbody = document.querySelector("tbody");
 
 // Get a reference to the columns
-var datetime = document.querySelector("#datetime")
-var city = document.querySelector("#city")
-var state = document.querySelector("#state")
-var country = document.querySelector("#country")
-var shape = document.querySelector("#shape")
-var duration = document.querySelector("#duration")
+var datetime = document.querySelector("#datetime");
+var city = document.querySelector("#city");
+var state = document.querySelector("#state");
+var country = document.querySelector("#country");
+var shape = document.querySelector("#shape");
+var duration = document.querySelector("#duration");
 
 // Get a reference to the search box and buttons
 var $searchInput = document.querySelector("#search");
 var $searchButton = document.querySelector("#searchButton");
 var $reloadButton = document.querySelector("#reloadButton");
 
-// Add a listener to the button so we can tell when it's clicked
+// get a reference to the filters
+var $dateFilter = document.querySelector("#filterDate");
+var $cityFilter = document.querySelector("#filterCity");
+var $stateFilter = document.querySelector("#filterState");
+var $countryFilter = document.querySelector("#filterCountry");
+var $durationFilter = document.querySelector("#filterDuration");
+
+
+// Add appropriate listeners to buttons so we can tell when it's clicked
 $searchButton.addEventListener("click", searchButtonClicked);
-
-// Add listener to reload dataset button
 $reloadButton.addEventListener("click", reloadButtonClicked);
+$dateFilter.addEventListener("click", filterSelected(datetime));
+$cityFilter.addEventListener("click", filterSelected(city));
+$stateFilter.addEventListener("click", filterSelected(state));
+$countryFilter.addEventListener("click", filterSelected(country));
+$durationFilter.addEventListener("click", filterSelected(duration));
 
-// Create a variable to reference the dataset
+// Create a variable to reference the dataset and the initial filter
 var dataset = dataSet
-
+var filterCriteria = datetime
 
 // Define a function to create the table from the data file
 function createTable() {
@@ -48,6 +59,12 @@ function createTable() {
     }
 }
 
+// function for selecting a filter
+function filterSelected(selectedFilter) {
+    filterCriteria = selectedFilter
+    console.log(filterCriteria)
+}
+
 // function for when you click the button 
 function searchButtonClicked() {
     dataset = dataSet
@@ -58,7 +75,7 @@ function searchButtonClicked() {
 
         // filter on date
         dataset = dataset.filter(function (row) {
-            return row.datetime === searchInput;
+            return row.filterCriteria === searchInput;
         });
 
     } catch {
